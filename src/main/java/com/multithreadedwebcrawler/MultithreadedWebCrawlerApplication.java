682c3cb53be0b06c2ca63f763bbf8c8ef1427c0f
Nihad <nihadamirov98@gmail.com>
2024-08-15 19:43:39 +0400

    init repo

---
+++ src/main/java/com/multithreadedwebcrawler/MultithreadedWebCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.multithreadedwebcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedWebCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadedWebCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multithreadedwebcrawler/MultithreadedWebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multithreadedwebcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedWebCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
