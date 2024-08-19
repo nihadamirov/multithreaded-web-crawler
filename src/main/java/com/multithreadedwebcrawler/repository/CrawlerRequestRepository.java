35162960b374a99446b033410c6358c64db6b1be
Nihad <nihadamirov98@gmail.com>
2024-08-19 21:58:35 +0400

    database config and create entity file

--- src/main/java/com/multithreadedwebcrawler/repository/CrawlerRequestRepository.java
+++ src/main/java/com/multithreadedwebcrawler/repository/CrawlerRequestRepository.java
@@ -1,10 +1,9 @@
 package com.multithreadedwebcrawler.repository;
 
-import com.multithreadedwebcrawler.model.JobData;
+import com.multithreadedwebcrawler.entity.CrawlerRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobDataRepository extends JpaRepository<JobData, Long> {
-
+public interface CrawlerRequestRepository extends JpaRepository<CrawlerRequest, Long> {
 }
